79fa6445ccd4ac83e64ecb692a64b4040afba9ca
Marco Chang <imnotmarco@live.com>
2015-06-23 23:51:15 -0400

    comments + readme file

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # slim-booboo
 Booboo's integration for Slim3.0 framework
+
+Install via Composer
+---------------------
+composer require marcoazn89/slim-booboo:dev-master
+
+Set up
+-------
+```php
+$app->add(new \SlimBooboo\Middleware());
+```
+
+Example
+--------
+```php
+<?php
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App();
+
+$app->add(new \SlimBooboo\Middleware());
+
+$app->get('/exception/', function($req, $res, $arg) {
+	throw new Exception("Error Processing Request", 1);
+});
+
+$app->get('/error/', function($req, $res, $arg) {
+	$a->B();
+});
+
+$app->run();
+```
--- src/Middleware.php
+++ src/Middleware.php
@@ -17,17 +17,22 @@ class Middleware {
 		$app = $next;
     $container = $app->getContainer();
 
+    // Start BooBoo
 		BooBoo::setUp();
 
+		// Overwrite the errorHandler
 		$container['errorHandler'] = function($c) {
 			return function($request, $response, $exception) {
 
+				// Store the BooBoo error body response in a buffer
 				ob_start();
 				BooBoo::exceptionHandler($exception);
 				$buffer = ob_get_contents();
 				ob_end_clean();
 
+				// By creating a new response object, all the headers set by BooBoo get resynced
 				$response = new \HTTP\Response();
+
 				return $response->overwrite($buffer);
 			};
 		};
